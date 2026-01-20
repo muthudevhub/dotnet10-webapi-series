7c791a38d075239b09ff34974ac41ccfabf504cd
muthudevhub <muthudevhub@gmail.com>
2026-01-20 22:26:13 +1300

    API Episode-2 file changes

--- MyFirstApi/Controllers/ProductsController.cs
+++ MyFirstApi/Controllers/ProductsController.cs
@@ -7,10 +7,24 @@ namespace MyFirstApi.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
+        [HttpGet("welcome")]
         public IActionResult WelcomeMessage()
         {
             return Ok("Welcome to Muthu Dev Hub Channel!");
         }
+        [HttpGet("All")]
+        public IActionResult GetAllProducts()
+        {
+            var products = new[]
+            {
+                new { Id = 1, Name = "Product 1", Price = 10.0m, Description = "Description for Product 1" },
+                new { Id = 2, Name = "Product 2", Price = 20.0m, Description = "Description for Product 2" },
+                new { Id = 3, Name = "Product 3", Price = 30.0m, Description = "Description for Product 3" }
+            };
+            return Ok(products);
+        }
+
+
+
     }
 }
